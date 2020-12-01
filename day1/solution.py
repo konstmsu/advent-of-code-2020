eb0a1dbb71b3c7445196f142ccdf769ede8368c2
Konstantin Spirin <konstmsu@gmail.com>
2020-12-01 21:07:54 +0800

    Solved day 1 part 1

--- day1/solution.py
+++ day1/solution.py
@@ -1,14 +1,16 @@
 # %%
-import numpy as np
+from typing import Set
 
 
-def solve1(target: int, count: int, numbers: List[int]) -> int:
-    nn = np.array(numbers)
-    sums2 = nn.T + nn
-    print(sums2 == target)
+def solve1(target: int, count: int, numbers: Set[int]) -> int:
+    for n in numbers:
+        if (target - n) in numbers:
+            return (target - n) * n
+
+    raise "Not found"
 
 
 with open("input.txt") as file:
     numbers = [int(line) for line in file.readlines()]
 
-print(solve1(2020, 2, set(), numbers))
+print(solve1(2020, 2, set(numbers)))
--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env zsh
 
-python3 -m venv venv
+python3.9 -m venv venv
 . ./venv/bin/activate
 pip install wheel
 pip install -r ./requirements.txt
