8b0e59a4492c18247aaaa9ad796fb45d3c1b0e6b
Konstantin Spirin <konstmsu@gmail.com>
2020-12-01 21:11:30 +0800

    Solved day 1 part 2

--- day1/solution.py
+++ day1/solution.py
@@ -2,7 +2,7 @@
 from typing import Set
 
 
-def solve1(target: int, count: int, numbers: Set[int]) -> int:
+def solve1(target: int, numbers: Set[int]) -> int:
     for n in numbers:
         if (target - n) in numbers:
             return (target - n) * n
@@ -10,7 +10,18 @@ def solve1(target: int, count: int, numbers: Set[int]) -> int:
     raise "Not found"
 
 
+def solve2(target: int, numbers: Set[int]) -> int:
+    for n1 in numbers:
+        for n2 in numbers:
+            n3 = target - n1 - n2
+            if n3 in numbers:
+                return n1 * n2 * n3
+
+    raise "Not found"
+
+
 with open("input.txt") as file:
     numbers = [int(line) for line in file.readlines()]
 
-print(solve1(2020, 2, set(numbers)))
+print(f"{solve1(2020, set(numbers))=}")
+print(f"{solve2(2020, set(numbers))=}")
