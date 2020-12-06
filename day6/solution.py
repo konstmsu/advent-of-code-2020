185f00b694fa6b9f88c4902405ddc2c786b3f428
Konstantin Spirin <konstmsu@gmail.com>
2020-12-06 13:17:26 +0800

    Day 6 part 2

--- day6/solution.py
+++ day6/solution.py
@@ -16,6 +16,29 @@ def solve1(lines):
             groupComplete()
         else:
             currentGroup = currentGroup.union(line)
+            print(f"{line=} {currentGroup=}")
+
+    groupComplete()
+
+    return sum(groups)
+
+
+def solve2(lines):
+    currentGroup = None
+    groups = []
+
+    def groupComplete():
+        nonlocal currentGroup
+        groups.append(len(currentGroup))
+        currentGroup = None
+
+    for line in lines:
+        if line == "":
+            groupComplete()
+        else:
+            currentGroup = (
+                set(line) if currentGroup is None else currentGroup.intersection(line)
+            )
 
     groupComplete()
 
@@ -25,4 +48,7 @@ def solve1(lines):
 with open("input.txt") as file:
     lines = file.read().splitlines()
 
-solve1(lines)
+# solve1(lines)
+print(solve2(lines))
+
+assert 3493 == solve2(lines)
